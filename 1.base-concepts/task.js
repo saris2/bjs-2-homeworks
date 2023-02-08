3cb7954113cd307bfbb21a6390d44335f38599e1
saris2 <98690721+saris2@users.noreply.github.com>
2023-02-08 16:50:49 +0300

    new solution

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,30 +1,53 @@
 "use strict"
-let solveEquation = (a, b, c) => {
+function solveEquation(a, b, c) {
   if (a == 0)
     return false;
-  let res = {};
+  let arr = {};
   let D = b * b - 4 * a * c;
   if (D < 0)
     return false;
   if (D == 0)
-    res['Корень уравнения'] = (-b + Math.sqrt(D)) / (2 * a);
+    arr['Корень уравнения'] = (-b + Math.sqrt(D)) / (2 * a);
   else if (D > 0) {
     let tmp = [];
     tmp.push((-b + Math.sqrt(D)) / (2 * a));
     tmp.push((-b - Math.sqrt(D)) / (2 * a));
-    res['2 корня уравнения'] = tmp;
+    arr['2 корня уравнения'] = tmp;
   }
-  return res;
+  return arr;
 }
 console.log(solveEquation(1, 5, 4));
 console.log(solveEquation(1, 2, 1));
 console.log(solveEquation(1, 2, 10));
 
-let calculateTotalMortgage = (percent, contribution, amount, countMonths)
-console.log(Number.isNaN(percent, contribution, amount, countMonths));
-let P = ((percent / 100) / 12);
-let S = (amount - contribution);
-let platezh = S * (P + (P / (((1 + P) ^ countMonths) - 1)));
-let all_prise = platezh * countMonths;
-console.log(calculateTotalMortgage(10, 0, 50000, 12));
-console.log(Number(platezh).toFixed(2));
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  // Контроль корректность введенных данных.    
+  if (percent > 0) {
+    percent = +percent;
+  } else if (percent != 0 || percent == 0) {
+    return (`“Параметр "Процентная ставка" содержит неправильное значение ${percent}”.`);
+  }
+
+  if (contribution >= 0) {
+    contribution = +contribution;
+  } else if (contribution != 0) {
+    return (`“Параметр "Начальный взнос" содержит неправильное значение ${contribution}”.`);
+  }
+
+  if (amount >= 0) {
+    amount = +amount;
+  } else if (amount != 0) {
+    return (`“Параметр "Общая стоимость" содержит неправильное значение ${amount}”.`);
+  }
+  // Расчет количества месяцев, на которые оформляется ипотека. 
+  let currentDate = new Date();  // Дата с какого числа берется ипотеки 
+  date = new Date(window.date.value); //Дата до какого числа берется ипотека
+  let payPeriod = 0 - ((currentDate.getFullYear() - date.getFullYear()) * 12) - (currentDate.getMonth() - date.getMonth());
+  date = payPeriod; // Количество месяцев, на которые оформляется ипотека.
+  let returnAmount = amount - contribution  // Сумма, которую необходимо вернуть банку. 
+  percent = percent / 1200; // Процентная ставка
+  let monthlyPay = amount * (percent + percent / (((1 + percent) ** date) - 1)); // Ежемесячная оплата
+  let totalAmount = monthlyPay * date;  //  общая сумма, которую придется заплатить клиенту.
+  console.log(totalAmount.toFixed(2));
+  return totalAmount.toFixed(2); //  Возврат результата в функцию
+}
