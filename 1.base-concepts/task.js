57a65ebbe7160939591b88ee9a5bb70074a4a575
saris2 <98690721+saris2@users.noreply.github.com>
2023-02-09 16:41:22 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,14 +4,14 @@ function solveEquation(a, b, c) {
   let arr = [];
   let x1;
   let x2;
-  let d = b ** 2 - 4 * a * c;
-  if (d < 0) {
+  const discriminant = b ** 2 - 4 * a * c;
+  if (discriminant < 0) {
     arr = [];
-  } else if (d === 0) {
-    arr = [(-b + Math.sqrt(d)) / (2 * a)];
+  } else if (discriminant === 0) {
+    arr = [(-b + Math.sqrt(discriminant)) / (2 * a)];
   } else {
-    x1 = (-b + Math.sqrt(d)) / (2 * a);
-    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
     arr.push(x1, x2);
   }
   return arr;
@@ -26,19 +26,19 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   if (percent > 0) {
     percent = +percent;
   } else if (percent != 0 || percent == 0) {
-    return (`“Параметр "Процентная ставка" содержит неправильное значение ${percent}”.`);
+    return false;
   }
 
   if (contribution >= 0) {
     contribution = +contribution;
   } else if (contribution != 0) {
-    return (`“Параметр "Начальный взнос" содержит неправильное значение ${contribution}”.`);
+    return false;
   }
 
   if (amount >= 0) {
     amount = +amount;
   } else if (amount != 0) {
-    return (`“Параметр "Общая стоимость" содержит неправильное значение ${amount}”.`);
+    return false;
   }
   percent = percent / 1200;
   let returnAmount = amount - contribution;
@@ -47,11 +47,10 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
   return totalAmount.toFixed(2);
 }
-
 console.log(calculateTotalMortgage('test', 1, 1, 1));
 console.log(calculateTotalMortgage(1, 'test', 1, 1));
 console.log(calculateTotalMortgage(1, 1, 'test', 1));
-console.log(calculateTotalMortgage(10, 0, 50000, 12));
+console.log(calculateTotalMortgage(Number("10"), Number("0"), Number("50000"), Number("12")));
 console.log(calculateTotalMortgage(10, 1000, 50000, 12));
 console.log(calculateTotalMortgage(10, 0, 20000, 24));
 console.log(calculateTotalMortgage(10, 1000, 20000, 24));
