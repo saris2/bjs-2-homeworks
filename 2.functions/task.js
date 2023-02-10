7901eea2d9bfcd3140cacfca96ecd1fd220a195c
saris2 <98690721+saris2@users.noreply.github.com>
2023-02-10 13:17:48 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,22 @@
-function getArrayParams(...arr) {
-
-  return { min: min, max: max, avg: avg };
+'use strict'
+// Задание 1
+function getArrayParams(arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  avg = Number(parseFloat((sum / arr.length).toFixed(2)));
+  return { min:min, max:max, avg:avg };
 }
-
-function summElementsWorker(...arr) {
-
-}
-
-function differenceMaxMinWorker(...arr) {
-
-}
-
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
+ console.log(getArrayParams([-99, 99, 10]));
+console.log(getArrayParams([1, 2, 3, -100, 10]));
+console.log(getArrayParams([5]));
