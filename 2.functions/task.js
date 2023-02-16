51367b702091ed3ba5e25067f79bf79e7dbdce63
saris2 <98690721+saris2@users.noreply.github.com>
2023-02-16 18:43:50 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,3 +1,4 @@
+// 1 задание
 
 function getArrayParams(...arr) {
   let min = Infinity;
@@ -20,7 +21,7 @@ console.log(getArrayParams(-99, 99, 10));
 console.log(getArrayParams(1, 2, 3, -100, 10));
 console.log(getArrayParams(5));
 
-
+// 2 задание
 
 
 function summElementsWorker(...arr) {
@@ -71,6 +72,7 @@ console.log(differenceEvenOddWorker(1, 2, 3, 4, 5, 6, 7, 8, 9));
 console.log(differenceEvenOddWorker(94, 51, 57, 41, 47, 66, 58, 10, 38, 17));
 console.log(differenceEvenOddWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35));
 
+
 function averageEvenElementsWorker(...arr) {
   let sumEvenElement = 0;
   let countEvenElement = 0;
@@ -86,3 +88,23 @@ function averageEvenElementsWorker(...arr) {
 console.log(differenceMaxMinWorker());
 console.log(averageEvenElementsWorker(1, 2, 3, 4, 5, 6, 7, 8, 9));
 console.log(averageEvenElementsWorker(15, 97, 85, 64, 67, 10, 69, 40, 15, 35));
+
+// 3 задание
+
+
+function makeWork(arrOfArr, func) {
+  let maxWorkerResult = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let arrSum = func(...arrOfArr[i]);
+    if (arrSum > maxWorkerResult) {
+      maxWorkerResult = arrSum;
+    }
+  }
+  return maxWorkerResult;
+}
+
+const arr = [[10, 10, 11, 20, 10], [67, 10, 2, 39, 88], [72, 75, 51, 87, 43], [30, 41, 55, 96, 62]];
+console.log(makeWork(arr, summElementsWorker));
+console.log(makeWork(arr, differenceMaxMinWorker)); 
+console.log(makeWork(arr, differenceEvenOddWorker)); 
+console.log(makeWork(arr, averageEvenElementsWorker)); 
